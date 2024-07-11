935e52bcdbe962a4ad9093b435b927ebb40ab5f8
Lanut1 <anna.tabakova27@gmail.com>
2024-07-11 23:18:35 +0200

    Complete the Odin Library project

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
+// Initialize an empty array to store the books
 const myLibrary = [];
 
+// Book constructor function
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
@@ -7,42 +9,60 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
+// Adding several initial books to the library
 const firstBook = new Book("Martin Eden", "Jack London", 393, "yes");
 const secondBook = new Book("The Count of Monte Cristo", "Alexandre Dumas", 1276, "in-progress");
 const thirdBook = new Book("Flowers for Algernon", "Daniel Keyes", 311, "yes");
-
 myLibrary.push(firstBook, secondBook, thirdBook);
+
+
+// Initial call to createLibrary to display the initial books
 createLibrary(myLibrary);
 
+
+//Creating library book cards
 function createLibrary(myLibrary) {
     const libraryCards = document.querySelector(".library-cards");
     libraryCards.replaceChildren();
 
+    // Iterate over all books in the myLibrary array 
     for (let book of myLibrary) {
+        // Create the book card container
         const bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
+    
+        // Container for icons
+        const iconsContainer = document.createElement("div");
+        iconsContainer.classList.add("icons-container");
 
+        // Container for settings icons (read status, delete)
+        const iconsSettings = document.createElement("div");
+        iconsSettings.classList.add("icons-settings");
+    
+        // Iterate over book elements and add them to the book card
         for (let element in book) {
             const elementContainer = document.createElement("div");
             elementContainer.classList.add(`book-${element}`);
-
+            
+            // Adding special icons for the number of pages
             if (element === "pages") {
                 const pagesIcon = document.createElement("span");
                 pagesIcon.classList.add("material-symbols-outlined");
                 pagesIcon.innerText = "library_books";
                 elementContainer.appendChild(pagesIcon);
-
+    
                 const pagesNumber = document.createElement("div");
                 pagesNumber.classList.add("pages-number");
                 pagesNumber.innerText = book[element];
                 elementContainer.appendChild(pagesNumber);
+    
+                iconsContainer.appendChild(elementContainer);
+
             } else if (element === "readStatus"){
-                const readStatusButton = document.createElement("button");
-                readStatusButton.classList.add("change-read-status");
-
+                // Adding read status symbol
                 const readSymbol = document.createElement("span");
-                readSymbol.classList.add("material-symbols-outlined");
-
+                readSymbol.classList.add("material-symbols-outlined", "change-read-status");
+    
                 if (book[element] === "yes") {
                     readSymbol.innerText = "check_circle";
                 } else if (book[element] === "in-progress") {
@@ -51,27 +71,74 @@ function createLibrary(myLibrary) {
                     readSymbol.innerText = "cancel";
                 }
 
-                readStatusButton.appendChild(readSymbol);
-                elementContainer.appendChild(readStatusButton);
+                elementContainer.appendChild(readSymbol);
+                iconsSettings.appendChild(elementContainer);
+
             }else {
+                // Adding book title and author name elements to the book card
                 elementContainer.innerText = book[element];
+                bookCard.appendChild(elementContainer);
             }
-            bookCard.appendChild(elementContainer);
         }
-
-        const deleteButton = document.createElement("button");
-        deleteButton.classList.add("delete-button");
+        
+        // Create delete icon
         const deleteIcon = document.createElement("span");
-        deleteIcon.classList.add("material-symbols-outlined");
+        deleteIcon.classList.add("material-symbols-outlined", "delete-button");
         deleteIcon.innerText = "delete";
-        deleteButton.appendChild(deleteIcon);
-        bookCard.appendChild(deleteButton);
+        iconsSettings.appendChild(deleteIcon);
+        iconsContainer.appendChild(iconsSettings);
 
+
+        bookCard.appendChild(iconsContainer);
         libraryCards.appendChild(bookCard);
     }
 }
 
+// Create event listeners for all cards
+const library = document.querySelector(".library-cards");
+library.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-button")) {
+        // Delete button click
+        const cardDelete = event.target.closest(".book-card");
+        const bookTitle = cardDelete.querySelector(".book-title").innerText;
+        const index = myLibrary.findIndex(book => book.title === bookTitle);
 
+        if (index !== -1) {
+            myLibrary.splice(index, 1);
+            cardDelete.remove();
+        } else {
+            console.error("The card is not found")
+        }
+    } else if (event.target.classList.contains("change-read-status")) {
+        // Change read status click
+        const cardChangeStatus = event.target.closest(".book-card");
+        const bookTitle = cardChangeStatus.querySelector(".book-title").innerText;
+        const index = myLibrary.findIndex(book => book.title === bookTitle);
+
+        if (index !== -1) {
+            let book = myLibrary[index];
+            switch(book.readStatus) {
+                case "yes":
+                    book.readStatus = "no";
+                    event.target.innerText = "cancel";
+                    break;
+                case "no":
+                    book.readStatus = "in-progress";
+                    event.target.innerText = "arrow_forward";
+                    break;
+                case "in-progress":
+                    book.readStatus = "yes";
+                    event.target.innerText = "check_circle";
+                    break;
+                default:
+                    console.error("Unknown read status");
+            }
+        }
+    }
+})
+
+
+// Show dialog window to add new book
 const showDialogButton = document.querySelector("#show-dialog");
 const dialogForm = document.querySelector("#add-book-dialog");
 const closeButton =document.querySelector("#close-dialog");
@@ -86,6 +153,8 @@ closeButton.addEventListener("click", () => {
     dialogForm.close();
 })
 
+
+// Add new book to the library
 bookForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -96,7 +165,9 @@ bookForm.addEventListener("submit", (event) => {
 
     const newBook = new Book(title, author, pages, readStatus);
     myLibrary.push(newBook);
+
     bookForm.reset();
     dialogForm.close();
+
     createLibrary(myLibrary);
 })
